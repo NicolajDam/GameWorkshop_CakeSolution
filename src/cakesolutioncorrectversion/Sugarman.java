2766113189c74868d55fe3e6ffe9592ada0aaae2
Nicolaj <nicolajdamfrederiksen@gmail.com>
2013-05-02 14:19:00 +0200

    Javadoc 1

--- src/cakesolutioncorrectversion/Sugarman.java
+++ src/cakesolutioncorrectversion/Sugarman.java
@@ -8,56 +8,90 @@ import cakesolutioncorrectversion.Players;
 import cakesolutioncorrectversion.PlayerController;
 import cakesolutioncorrectversion.Location.Direction;
 
-/**
- *
- * @author anders
+/** Sugarman is a class that extends Players and implements PlayerController.
+ * The class contains the methods needed for the player to play the game, 
+ * such as keeping score (the sugar level), eating or not eating the cakes and 
+ * the location of the player.
+ * @author CakeSolutionGroup
+ * @version 1.0 (2 May 2013)
  */
 public class Sugarman extends Players implements PlayerController {
 
     private Location sugarmanLocation;
     private int currentSugarLevel = 50;
 
+    /**move is a method that determines whether the player can 
+     * move to the desired location.
+     * @param direction
+     * @return boolean
+     */
     @Override
     public boolean move(Direction direction) {
         //TODO implement it
         return false;
     }
 
+    /**getSugarLevel returns the currentSugarLevel, which keeps track of the 
+     * player's progress
+     * @param null
+     * @returns the currentSugarLevel of type integer 
+     */
     @Override
     public int getSugarlevel() {
 
         return currentSugarLevel;
     }
 
+    /** changeSugarLevel changes the currentSugarLevel
+     * @param difference, which is an integer variable
+     * @return Null
+     */
     @Override
     public void changeSugarLevel(int difference) {
 
         currentSugarLevel = currentSugarLevel + difference;
     }
 
+    /** eatCake sets the value for the integer 'difference' in the method
+     * changeSugarLevel, whenever the player answers a question correctly
+     * @param cake of class type Cake 
+     * @return Null
+     */
     public void eatCake(Cake cake) {
         changeSugarLevel(cake.sugarLevel);
     }
 
+    /** dontEatCake sets the value for the integer 'difference' in the method
+     * changeSugarLevel, whenever the player answers a question incorrectly
+     * @param cake of class type Cake 
+     * @return Null
+     */
     @Override
     public void dontEatCake(Cake cake) {
         changeSugarLevel(-cake.sugarLevel);
 
     }
 
+    /** getLocation returns the current location of sugarman
+     * @param Null
+     * @returns sugarmanLocation of class type Location 
+     */
     @Override
     public Location getLocation() {
         return sugarmanLocation;
-        //throw new UnsupportedOperationException("Not supported yet.");
+       
     }
 
+    /** setLocation returns true whenever the location equals sugarmanLocation
+     * 
+     * @param location of class type Location
+     * @return boolean
+     */
     @Override
     public boolean setLocation(Location location) {
-        //Methode variable: Location som vi kalder sugarmanLocation.
+       
         sugarmanLocation = location;
         return true;
 
-
-        //throw new UnsupportedOperationException("Not supported yet.");
     }
 }
--- src/cakesolutioncorrectversion/SuperLocation.java
+++ src/cakesolutioncorrectversion/SuperLocation.java
@@ -9,36 +9,56 @@ import cakesolutioncorrectversion.Location.Direction;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author eskandarpahlavaafshari
+/** The class 'SuperLocation' is a super class, which implements Location.
+ * It's purpose is to create the methods for placing the locations in relation
+ * to each other. It also contains methods that hold information about the 
+ * different locations.
+ * @author CakeSolutionGroup
+ * @version 1.0 (1 May 2013)
  */
 abstract class SuperLocation implements Location{
     
      List<Location> myLocations = new ArrayList<Location>();
     
      
-     
      public Location neighborEast;
      public Location neighborWest; 
      
     
-
+/**
+ * @param direction
+ * @returns the exists from the current location, the locations are of type 
+ * Location class 
+ */
     @Override
     public List<Location> getExits(Direction direction) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * 
+     * @returns a string, which describes the name of the location
+     */
     @Override
     public String getDescription() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
+    
+    /**
+     * 
+     * @param l 
+     * @return null
+     */
     @Override
     public void setEastNeighbor(Location l) {
         this.neighborEast = l;
     }
-     @Override
+    /**
+     * @param null
+     * @return null
+     */ 
+    
+    @Override
     public Location getEastNeighbor(){
         return null;
         
